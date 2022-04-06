68ccf8a44959330d7764709222eae194e5cc4eb7
JeskaLopes <jessica97cantora@hotmail.com>
2022-04-06 11:51:15 -0300

    Segunda parte security

--- src/main/java/br/org/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/br/org/generation/blogpessoal/service/UsuarioService.java
@@ -18,58 +18,62 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	
-	public Optional<Usuario>cadastrarUsuario(Usuario usuario){
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+			return Optional.empty();
 		
-		if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
-			return Optional.empty();						
-		}
-			
 		usuario.setSenha(criptografarSenha(usuario.getSenha()));
-		
+
 		return Optional.of(usuarioRepository.save(usuario));
+	
 	}
-	
-	
-	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin){
-		
+
+
+	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
-		
-		if(usuario.isPresent()) {
-			if(compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())){
+
+		if (usuario.isPresent()) {
+			if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
+
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setFoto(usuario.get().getFoto());
+				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
-				usuarioLogin.get().setToken(geradorBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
-	
-				
+
 				return usuarioLogin;
+
 			}
-		}
+		}	
 		
 		return Optional.empty();
+		
 	}
-	
-	
-	
-	private boolean compararSenhas(String senhaDigitada, String senhaDoBanco) {
+
+	private String criptografarSenha(String senha) {
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		return encoder.matches(senhaDigitada, senhaDoBanco);
-	}
-	
-	private String criptografarSenha(String senha) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder.encode(senha);
+
 	}
 	
-	private String geradorBasicToken(String usuario, String senha) {
+	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		return encoder.matches(senhaDigitada, senhaBanco);
+
+	}
+
+	private String gerarBasicToken(String usuario, String senha) {
 
 		String token = usuario + ":" + senha;
 		byte[] tokenBase64 = Base64.encodeBase64(token.getBytes(Charset.forName("US-ASCII")));
 		return "Basic " + new String(tokenBase64);
+
 	}
-	
-	
+
 }
